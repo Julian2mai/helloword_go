3693450903537c59dbe42332df55fc03b698056c
Julian2mai <julimaika2003@gmail.com>
2024-09-24 13:40:26 -0500

    dedicatoria 1

--- ejercicio1.go
+++ ejercicio1.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+)
 
 func imprimir() {
 	fmt.Println("texto desdre la funcion imprimir")
@@ -22,10 +26,46 @@ func comparar_bool() {
 		fmt.Println("A Y B no son iguales ")
 	}
 }
+
+func arreglo() {
+	var aprendices [5]string
+	aprendices[0] = "Mairongo"
+	aprendices[1] = "Eduardo"
+	aprendices[2] = "camilo"
+	aprendices[3] = "julian"
+	aprendices[4] = "andres"
+	fmt.Println(aprendices[0])
+}
+
+func tipo_datos() {
+	var texto string = "mairongo"
+	var entero int = 1000
+	var decimal float64 = 0.00001
+	fmt.Println(reflect.TypeOf(texto))
+	fmt.Println(reflect.TypeOf(entero))
+	fmt.Println(reflect.TypeOf(decimal))
+}
+
+func convertirganado (){
+	var palabra string = "true"
+	boolean, _ := strconv.ParseBool(palabra)
+	fmt.Println(boolean, reflect.TypeOf(boolean))
+	}
+
+func convernovia () {
+	var palabra_bool bool =true
+	strbool := strconv.FormatBool((palabra_bool))
+	fmt.Println(strbool, reflect.TypeOf(strbool))
+}
+
 func main() {
 	// imprimir()
 	// fmt.Print("texto desde la fincion main")
-	// çfmt.Println(hola_string(" mairongo"))
+	// fmt.Println(hola_string(" mairongo"))
 	// fmt.Println(sumar(5, 3))
-	comparar_bool()
+	// comparar_bool()
+	// arreglo()
+	// tipo_datos()
+    // convertirganado()
+	convernovia()
 }
